e4239658b1f281da22cb17bff95b6fde57f5fa1c
bwnstk <info@flimmerkiste.xyz>
2020-12-04 21:13:00 +0100

    :construction: Thinking...

--- day3.js
+++ day3.js
@@ -1,3 +1,20 @@
 const fs = require("fs");
 const entries = fs.readFileSync("./day3.txt", "utf-8");
 const entryArray = entries.split("\n");
+let height = 30;
+let counter = 0;
+let splitline = "";
+for (let i = 0; i <= 100; i++) {
+  splitline = entryArray[i].split("");
+  if (splitline[height] === "#") {
+    counter++;
+  }
+  console.log(splitline[height]);
+  i = i + 3;
+  height--;
+  if (height < 0) {
+    return;
+  }
+  return true;
+}
+console.log(counter);
