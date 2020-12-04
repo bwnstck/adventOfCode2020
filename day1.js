874a34b690dd9b5d2a6706366b9784dee7b36557
bwnstk <info@flimmerkiste.xyz>
2020-12-04 01:20:13 +0100

    :sparkles: Solving both puzzles with forEach
    their should be an more elegant version of this

--- day1.js
+++ day1.js
@@ -1,13 +1,17 @@
 const fs = require("fs");
 const numbers = fs.readFileSync("./day1.txt", "utf-8");
 const numbersArr = numbers.split("\n");
-
 const winnerArray = [];
-// numbersArr.forEach((element) => {
-//   numbersArr.forEach((secNum) => {
-//     if (element + secNum === 2020) {
-//       winnerArray.push(element, secNum);
-//     }
-//   });
-// });
+numbersArr.forEach((firstNum) => {
+  numbersArr.forEach((secNum) => {
+    numbersArr.forEach((thirdNum) => {
+      if (parseInt(firstNum) + parseInt(secNum) + parseInt(thirdNum) === 2020) {
+        return winnerArray.push(firstNum, secNum, thirdNum);
+      }
+    });
+  });
+});
+
 console.log({ winnerArray });
+const magicNumber = winnerArray[0] * winnerArray[1] * winnerArray[2];
+console.log(magicNumber);
