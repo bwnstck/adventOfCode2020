7c29f2f63e6d34e1532099827be0af417aadc7fd
bwnstk <info@flimmerkiste.xyz>
2020-12-04 22:56:51 +0100

    :construction: peeehhhh

--- day3.js
+++ day3.js
@@ -1,20 +1,31 @@
 const fs = require("fs");
 const entries = fs.readFileSync("./day3.txt", "utf-8");
 const entryArray = entries.split("\n");
-let height = 30;
+let height = entryArray.length;
+let leftRight = 0;
 let counter = 0;
-let splitline = "";
-for (let i = 0; i <= 100; i++) {
-  splitline = entryArray[i].split("");
-  if (splitline[height] === "#") {
+
+console.log({ height });
+
+for (let i = 0; i <= height; i++) {
+  const line = entryArray[i].split("");
+
+  if (line[leftRight] === "#") {
     counter++;
+    console.log(`Line ${i + 1} at ${leftRight + 1} || Found ${counter} tree `);
+  } else {
+    console.log(`Line ${i + 1} at ${leftRight + 1} || Found nothing :( `);
   }
-  console.log(splitline[height]);
-  i = i + 3;
+  leftRight = leftRight + 3;
+  if (leftRight > 31) {
+    const rest = leftRight % 31;
+    leftRight = rest;
+  }
+
   height--;
   if (height < 0) {
+    console.log("ENDE");
     return;
   }
-  return true;
 }
-console.log(counter);
+console.log({ counter }, "not 5, 22,83");
