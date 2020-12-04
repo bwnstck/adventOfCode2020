85ef47eb02d4c48ce7d8b8099512cd484fb203d2
bwnstk <info@flimmerkiste.xyz>
2020-12-04 19:55:24 +0100

    :sparkles: Finish Day2

--- day2.js
+++ day2.js
@@ -1,25 +1,43 @@
 const fs = require("fs");
 const entries = fs.readFileSync("./day2.txt", "utf-8");
 const entryArray = entries.split("\n");
-let validcounter = 0;
+let counter = 0;
+
 entryArray.forEach((entry) => {
   const testRangeArr = /\d+.\d+/g.exec(entry);
   const testRange = testRangeArr[0];
   const testRangeArrSplit = testRange.split("-");
-  const test1 = testRangeArrSplit[0];
-  const test2 = testRangeArrSplit[1];
+  const stringIn = /[a-zA-Z]{2,}/.exec(entry);
 
   const indicatorIn = /\w:/.exec(entry);
   const indicatorArr = indicatorIn[0].split(":");
-  const indicator = indicatorArr[0];
 
-  const stringIn = /[a-zA-Z]{2,}/.exec(entry);
-  const String = stringIn[0];
-  console.log(indicator, test1, test2, String);
-  const regex = new RegExp(`${indicator}{${test1},${test2}}`);
+  const min = parseInt(testRangeArrSplit[0]);
+  const max = parseInt(testRangeArrSplit[1]);
+  const letter = indicatorArr[0];
 
-  if (regex.test(String)) {
-    validcounter++;
+  const string = stringIn[0];
+  const stringArr = string.split("");
+  // console.log(letter, stringArr);
+  if (stringArr[min - 1] === letter) {
+    if (stringArr[max - 1] !== letter) {
+      console.log(letter, min, max, stringArr);
+      counter++;
+    }
   }
+  if (stringArr[max - 1] === letter) {
+    if (stringArr[min - 1] !== letter) {
+      console.log(letter, min, max, stringArr);
+      counter++;
+    }
+  }
+
+  // const resultArray = stringArr.filter((entry) => {
+  //   return entry === letter;
+  // });
+  //   console.log(min, resultArray.length, max);
+  // if (resultArray.length >= min && resultArray.length <= max) {
+  //   counter++;
+  // }
 });
-console.log(validcounter);
+console.log(counter);
